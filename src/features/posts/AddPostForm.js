5ebf6ad36445f78d0f0a6b6e3433def6dbb987b3
Tri Hung <koikoidth12@gmail.com>
2023-07-20 23:30:44 +0700

    Finish adding new posts

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+
+import { postAdded } from "./postsSlice";
 
 export const AddPostForm = () => {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
+    const dispatch = useDispatch();
+
     const onTitleChanged = (e) => setTitle(e.target.value);
     const onContentChanged = (e) => setContent(e.target.value);
+
+    const onSavePostClicked = () => {
+        if (title && content) {
+            dispatch(
+                postAdded({
+                    id: nanoid(),
+                    title,
+                    content,
+                })
+            );
+
+            setTitle("");
+            setContent("");
+        }
+    };
 
     return (
         <section>
@@ -13,6 +34,7 @@ export const AddPostForm = () => {
             <form>
                 <label htmlFor="postTitle">Post Title:</label>
                 <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     type="text"
                     id="postTitle"
                     name="postTitle"
@@ -21,12 +43,15 @@ export const AddPostForm = () => {
                 />
                 <label htmlFor="postContent">Content:</label>
                 <textarea
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="postContent"
                     name="postContent"
                     value={content}
                     onChange={onContentChanged}
                 />
-                <button type="button">Save Post</button>
+                <button type="button" onClick={onSavePostClicked}>
+                    Save Post
+                </button>
             </form>
         </section>
     );
