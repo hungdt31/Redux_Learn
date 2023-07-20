cf5997cf54082a3d3521a520a6e5d0706716cefe
Tri Hung <koikoidth12@gmail.com>
2023-07-20 23:08:40 +0700

    Showing the Posts List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
+import { Navbar } from "./app/Navbar";
+import { PostsList } from "./features/posts/PostsList";
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div className="App">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <React.Fragment>
+                                <PostsList />
+                            </React.Fragment>
+                        }
+                    />
+                    <Route path="/*" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,15 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const postSlice = createSlice({
-    name: 'posts',
-    initialState: [
-        {
-            id: '1', title: 'First Post!', content: 'Hello!'
-        },
-        {
-            id: '2', title: 'Second Post', content: 'More text'
-        }
-    ],
-    reducers:{}
-})
-export default postSlice.reducer
+const initialState = [
+    { id: "1", title: "First Post!", content: "Hello!" },
+    { id: "2", title: "Second Post", content: "More text" },
+];
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {},
+});
+
+export default postsSlice.reducer;
